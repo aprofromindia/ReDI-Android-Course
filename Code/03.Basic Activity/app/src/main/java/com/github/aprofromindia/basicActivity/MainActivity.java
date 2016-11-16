449ac4164fd34c2dc6720fe6c6213f37bd872dc2
Apro <aprofromuk-mail@yahoo.co.in>
2016-11-16 13:00:26 +0100

    basic activity initial commit.

---
+++ Code/03.Basic Activity/app/src/main/java/com/github/aprofromindia/basicActivity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.github.aprofromindia.basicActivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupMainBtn();
+    }
+
+    private void setupMainBtn() {
+        Button button = (Button) findViewById(R.id.main_btn);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DetailActivity.class));
+            }
+        });
+    }
+}
